5779b45e376b285c4178880afe751ac910c1d484
Juniper <juniperwrenarts@gmail.com>
2024-03-03 00:32:25 -0500

    added better notes for team

--- src/Components/home/home.jsx
+++ src/Components/home/home.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom - this was needed for button to work
 import './home.css'; // Import CSS file for styling
 
 function Home() {
   return (
     <div className="home-container">
       <div className="card-container">
+        {/* left box content */}
         <div className="left-card">
           <h2>Welcome to [Name of Website]</h2>
           <p>Where we believe in empowering non-profits through art.</p>
           <p>At [Art Auction Website Name], we believe in the transformative power of art to inspire, uplift, and create positive change. A portion of the proceeds from every auction goes directly to supporting non-profit organizations dedicated to promoting arts education, cultural enrichment, and community development.</p>
         </div>
+        {/* right box content */}
         <div className="right-card">
-          {/* Content for the second card */}
           <h3>Discover our Premier Auction Collection</h3>
           <Link to="/buy">
-            <button className="button">View & Bid</button> {/* Wrap the link inside a button */}
+            <button className="button">View & Bid</button> 
           </Link>
         </div>
       </div>
