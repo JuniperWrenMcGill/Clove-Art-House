63474b20f06d10c9024b1c718a6921cc2c88fc1d
Juniper <juniperwrenarts@gmail.com>
2024-03-04 00:55:46 -0500

    added impact jsx and css + google font to project

--- src/Components/impact/impact.jsx
+++ src/Components/impact/impact.jsx
@@ -2,15 +2,24 @@ import React from 'react';
 import './impact.css'; // Import the CSS file
 
 const ImpactPage = () => {
+  // Function to handle button click and redirect to another page
+  const handleButtonClick = () => {
+    window.location.href = 'https://example.com'; // Replace with the actual URL
+  };
+
   return (
     <div className="impact-page">
-      <h2>Our Impact</h2>
-      <p>We are proud to support the following nonprofit organizations with the proceeds from our sales:</p>
-      <ul>
-        <li>Nonprofit Organization 1</li>
-        
-        {/* Add more nonprofit organization info */}
-      </ul>
+      <div className="impact-left-card">
+        <h1 className="impact-title">Auction Proceeds Benefit the Featured Non-Profit:</h1>
+        <h2 className="impact-subtitle">Carolina Food Relief Initiative</h2>
+        <p className="impact-text">The Carolina Food Relief Initiative is dedicated to combating food insecurity and hunger in communities across North Carolina. Established in 2012, this organization is driven by the belief that every individual deserves access to nutritious food, regardless of their circumstances.</p>
+        <p className="impact-text">Join in the mission to nourish our neighbors and build a hunger-free North Carolina where everyone has access to nutritious food and the opportunity to thrive.</p>
+      </div>
+      <div className="impact-right-card">
+        {/* this needs to be made functional */}
+        <p className="impact-text-right">To learn more about this organization, please visit their website to explore their mission, programs, and impact, as well as opportunities to get involved or support their cause.</p>
+        <button className="impact-button" onClick={handleButtonClick}>Visit their website</button>
+      </div>
     </div>
   );
 };
