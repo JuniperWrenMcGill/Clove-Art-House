b42de7a26f0de3c1bbbcdc2c1656a7fec76d8292
Morganton <chadandari@outlook.com>
2024-03-04 10:59:00 -0500

    Reorganized file structure

--- controllers/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const apiRoutes = require('./api');
-const routes = require('./api/routes');
-
-router.use('/api', apiRoutes);
-router.use('/', routes);
-
-module.exports = router;
