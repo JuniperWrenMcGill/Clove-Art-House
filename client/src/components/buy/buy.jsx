da9dd547aff6dacc39a6f9c52925457907aaada2
Juniper <juniperwrenarts@gmail.com>
2024-03-05 18:35:23 -0500

    hardcoded content and styled buy page

--- client/src/components/buy/buy.jsx
+++ client/src/components/buy/buy.jsx
@@ -9,7 +9,7 @@ function Auction() {
   const [price, setPrice] = useState();
   // useState to manage bid, description, title, and image
   const [bid, setBid] = useState("");
-  const [description, setDescription] = useState("Step into the captivating world of an exquisite oil painting that exudes warmth and serenity. At the heart of the canvas stands a solitary figure, a gentleman adorned in a distinguished hat, lost in contemplation. The artist's masterful use of yellows, ranging from soft ochres to vibrant golds, infuses the scene with a luminous quality that captivates the eye. As light and shadow dance across the canvas, the depth and realism of the composition are enhanced, drawing the viewer deeper into the tranquil atmosphere. This piece invites you to immerse yourself in its rich hues and ponder the mysteries hidden within its depths. This remarkable artwork promises to be a cherished addition to any collection, offering a timeless reminder of the beauty found in moments of quiet reflection");
+  const [description, setDescription] = useState("Step into the captivating world of an exquisite oil painting. The artist's use of yellows infuses the scene with a luminous quality that captivates the eye. This remarkable artwork promises to be a cherished addition to any collection, offering a timeless reminder of the beauty found in moments of quiet reflection");
   const [title, setTitle] = useState("The Straw Hat by Nikos Lytras");
   const [imageURL, setImageURL] = useState("");
   //gets price from server
@@ -29,17 +29,19 @@ function Auction() {
 
   }
   return (
+    <div className="buy-page">
     <div className="App">
-      <div className="product-details">
+      <div className="product-details-card">
         <h2 className="product-title">{title}</h2>
         <p className="product-description">{description}</p>
-        <div className="product-image" />
+        <div className="product-image"/>
       </div>
       <div className="bid-section">
         <h3 className="bid-title">Latest Bid: ${price}</h3>
         <input id="bid" value={bid} placeholder="Place your bid!" className="bid-input" />
         <button onClick={checkBid} className="bid-submit">Submit</button>
       </div>
+    </div>
     </div>
   );
 }
