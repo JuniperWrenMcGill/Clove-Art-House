de688d517ef61607e9a07edf3166284952a1e46a
CodeClass0 <chadandari@outlook.com>
2024-03-06 13:37:01 -0500

    login works

--- server/routes/api/userRoutes.js
+++ server/routes/api/userRoutes.js
@@ -4,7 +4,7 @@ const { User } = require('../../models');
 
 
 // // CREATE a user
-router.post('/', async (req, res) => {
+router.post('/signup', async (req, res) => {
   try {
     const userData = await User.create(req.body);
     console.log(userData.id);
@@ -19,5 +19,36 @@ router.post('/', async (req, res) => {
   }
 });
 
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.loggedIn = true;
+
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 module.exports = router;
